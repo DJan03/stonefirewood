9bb30629dcb62ca329ebf7070657b6518964120d
DJan03 <danil2003nsk@gmail.com>
2021-08-27 17:40:26 +0300

    v0.0.1 Add Colors

--- src/main.c
+++ src/main.c
@@ -4,28 +4,57 @@
 #define CONSOLE_WIDTH 60
 #define CONSOLE_HEIGHT 30
 
-HANDLE MYHANDLE;
+#define BLACK 0
+#define DARK_BLUE 1
+#define DARK_GREEN 2
+#define DARK_CYAN 3
+#define DARK_RED 4
+#define DARK_PURPLE 5
+#define DARK_YELLOW 6
+#define LIGHT_GRAY 7
+#define GRAY 8
+#define BLUE 9
+#define GREEN 10
+#define CYAN 11
+#define RED 12
+#define PURPLE 13
+#define YELLOW 14
+#define WHITE 15
+
+HANDLE MY_HANDLE;
 
 void SetWindowSize()
 {
 	COORD coord = {};
 	coord.X = (SHORT)(CONSOLE_WIDTH);
 	coord.Y = (SHORT)(CONSOLE_HEIGHT);
-	SetConsoleScreenBufferSize(MYHANDLE, coord);
+	SetConsoleScreenBufferSize(MY_HANDLE, coord);
 
 	SMALL_RECT rect = {};
 	rect.Bottom = coord.Y - 1;
 	rect.Right = coord.X - 1;
-	SetConsoleWindowInfo(MYHANDLE, TRUE, &rect);
+	SetConsoleWindowInfo(MY_HANDLE, TRUE, &rect);
+}
+
+void Print(const char *message, int color)
+{
+	SetConsoleTextAttribute(MY_HANDLE, color);
+	printf("%s", message);
 }
 
 int main()
 {
-	MYHANDLE = GetStdHandle(STD_OUTPUT_HANDLE);
+	MY_HANDLE = GetStdHandle(STD_OUTPUT_HANDLE);
 
 	SetWindowSize();
 
-	printf("Hello world!\n");
+	Print("Hello ", WHITE);
+	Print("W", RED);
+	Print("O", DARK_YELLOW);
+	Print("R", YELLOW);
+	Print("L", GREEN);
+	Print("D", CYAN);
+	Print("!", BLUE);
 
 	getchar();
 	return 0;
