31f223cdea633fc26269f81d7fb053145e66d4a3
DJan03 <danil2003nsk@gmail.com>
2021-08-28 09:50:15 +0300

    v0.0.7 Player movement, render and test UI

--- src/main.c
+++ src/main.c
@@ -33,61 +33,45 @@
 
 #define ENTITY_CAPACITY 5
 
-typedef struct Ssprite
+
+typedef struct Sobject
 {
 	char symbol;
 	int color;
-} Tsprite;
+} Tobject;
 
-Tsprite MY_BUFFER[CONSOLE_HEIGHT][CONSOLE_WIDTH];
-bool NEED_TO_UPDATE[CONSOLE_HEIGHT][CONSOLE_WIDTH];
-
-HANDLE MY_HANDLE;
-
-enum Eenviroment
+typedef enum Eenvironment
 {
 	GRASS,
 	ROCK,
 	WATER
-};
+} TenvType;
 
-typedef struct Senviroment
+typedef struct Senvironment
 {
-	char symbol;
-	int color;
-	int type;
-} Tenviroment;
+	Tobject object;
+	TenvType type;
+} Tenvironment;
 
 typedef struct Sentity
 {
-	char symbol;
-	int color;
+	Tobject object;
 	int x;
 	int y;
 } Tentity;
 
+
+Tobject MY_BUFFER[CONSOLE_HEIGHT][CONSOLE_WIDTH];
+bool NEED_TO_UPDATE[CONSOLE_HEIGHT][CONSOLE_WIDTH];
+
+HANDLE MY_HANDLE;
+
 int inventory_id = 0;
 
-Tenviroment enviroment_map[MAP_HEIGHT][MAP_WIDTH];
+Tenvironment environment_map[MAP_HEIGHT][MAP_WIDTH];
 
 Tentity entity_map[ENTITY_CAPACITY];
 
-void SetBuffer(int x, int y, char symbol, int color)
-{
-	if (MY_BUFFER[y][x].symbol == symbol && MY_BUFFER[y][x].color == color)
-		return ;
-	NEED_TO_UPDATE[y][x] = TRUE;
-	MY_BUFFER[y][x].symbol = symbol;
-	MY_BUFFER[y][x].color = color;
-}
-
-void SetCursorPosition(int x, int y)
-{
-	COORD coord = {0};
-	coord.X = x;
-	coord.Y = y;
-	SetConsoleCursorPosition(MY_HANDLE, coord);
-}
 
 void SetWindowSize()
 {
@@ -102,77 +86,79 @@ void SetWindowSize()
 	SetConsoleWindowInfo(MY_HANDLE, TRUE, &rect);
 }
 
-void BuildEnviromentMap()
+void SetCursorPosition(int x, int y)
+{
+	COORD coord = {0};
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(MY_HANDLE, coord);
+}
+
+
+void PutObjectToBuffer(int x, int y, Tobject object)
+{
+	if (MY_BUFFER[y][x].symbol == object.symbol && MY_BUFFER[y][x].color == object.color)
+		return ;
+	NEED_TO_UPDATE[y][x] = TRUE;
+	MY_BUFFER[y][x] = object;
+}
+
+void PutObjectToConsole(int x, int y, Tobject object)
+{
+	SetCursorPosition(x, y);
+	SetConsoleTextAttribute(MY_HANDLE, object.color);
+	printf("%c", object.symbol);
+	NEED_TO_UPDATE[y][x] = FALSE;
+}
+
+
+int InBounds(int x, int y)
+{
+	return (x >= 0 && y >= 0 && x < MAP_WIDTH && y < MAP_HEIGHT);
+}
+
+
+void BuildEnvironmentMap()
 {
 	for (int i = 0; i < MAP_HEIGHT; i++)
 		for (int j = 0; j < MAP_WIDTH; j++)
 		{
 			if (j + i / 2 + rand() % 2 < 15)
 			{
-				enviroment_map[i][j].symbol = rand() % 2 ? '~' : '=';
-				enviroment_map[i][j].color = (i + j / 10) % 2 ? CYAN : BLUE;
-				enviroment_map[i][j].type = WATER;
+				environment_map[i][j].object.symbol = rand() % 2 ? '~' : '=';
+				environment_map[i][j].object.color = (i + j / 10) % 2 ? CYAN : BLUE;
+				environment_map[i][j].type = WATER;
 			}
 			else if (rand() % 100 < 5)
 			{
-				enviroment_map[i][j].symbol = rand() % 2 ? 'o' : 'n';
-				enviroment_map[i][j].color = rand() % 2 ? GRAY : LIGHT_GRAY;
-				enviroment_map[i][j].type = ROCK;
+				environment_map[i][j].object.symbol = rand() % 2 ? 'o' : 'n';
+				environment_map[i][j].object.color = rand() % 2 ? GRAY : LIGHT_GRAY;
+				environment_map[i][j].type = ROCK;
 			}
 			else
 			{
-				enviroment_map[i][j].symbol = rand() % 2 ? '\"' : '^';
-				enviroment_map[i][j].color = rand() % 2 ? GREEN : DARK_GREEN;
-				enviroment_map[i][j].type = GRASS;
+				environment_map[i][j].object.symbol = rand() % 2 ? '\"' : '^';
+				environment_map[i][j].object.color = rand() % 2 ? GREEN : DARK_GREEN;
+				environment_map[i][j].type = GRASS;
 			}
 		}
 }
 
-void PrintSymbol(int x, int y, char symbol, int color)
+void BuildEntityMap()
 {
-	SetCursorPosition(x, y);
-	SetConsoleTextAttribute(MY_HANDLE, color);
-	printf("%c", symbol);
-	NEED_TO_UPDATE[y][x] = FALSE;
-}
-
-int IsEntityCell(int x, int y)
-{
-	for (int i = 0; i < ENTITY_CAPACITY; i++)
+	entity_map[0].x = MAP_WIDTH / 2;
+	entity_map[0].y = MAP_HEIGHT / 2;
+	entity_map[0].object.symbol = '@';
+	entity_map[0].object.color = YELLOW;
+	for (int i = 1; i < ENTITY_CAPACITY; i++)
 	{
-		if (entity_map[i].x == x && entity_map[i].y == y)
-			return (i);
-	}
-	return (-1);
-}
-
-void PrintEnviromentMap()
-{
-	int entity_id;
-	for (int i = 0; i < MAP_HEIGHT; i++)
-	{
-		for (int j = 0; j < MAP_WIDTH; j++)
-		{
-			entity_id = IsEntityCell(j, i);
-			if (entity_id >= 0)
-				SetBuffer(j, i + 6, entity_map[entity_id].symbol, entity_map[entity_id].color);
-			else
-			{
-				SetBuffer(j, i + 6, enviroment_map[i][j].symbol, enviroment_map[i][j].color);
-				if (enviroment_map[i][j].type == WATER)
-					enviroment_map[i][j].color = rand() % 2 ? CYAN : BLUE;
-				if (enviroment_map[i][j].type == GRASS)
-					enviroment_map[i][j].color = rand() % 2 ? GREEN : DARK_GREEN;
-					
-			}
-		}
+		entity_map[i].x = MAP_WIDTH / 2 + rand() % 10 - 5;
+		entity_map[i].y = MAP_HEIGHT / 4 + rand() % 10 - 5;
+		entity_map[i].object.symbol = 'h';
+		entity_map[i].object.color = DARK_YELLOW;
 	}
 }
 
-int InBounds(int x, int y)
-{
-	return (x >= 0 && y >= 0 && x < MAP_WIDTH && y < MAP_HEIGHT);
-}
 
 void MoveEntity(Tentity *entity, int dx, int dy)
 {
@@ -181,26 +167,11 @@ void MoveEntity(Tentity *entity, int dx, int dy)
 
 	if (InBounds(newx, newy))
 	{
-		if (enviroment_map[newy][newx].type == GRASS)
+		if (environment_map[newy][newx].type == GRASS)
 		{
 			entity->x = newx;
 			entity->y = newy;
 		}
-	}
-}
-
-void BuildEntityMap()
-{
-	entity_map[0].x = MAP_WIDTH / 2;
-	entity_map[0].y = MAP_HEIGHT / 2;
-	entity_map[0].symbol = '@';
-	entity_map[0].color = YELLOW;
-	for (int i = 1; i < ENTITY_CAPACITY; i++)
-	{
-		entity_map[i].x = MAP_WIDTH / 2 + rand() % 10 - 5;
-		entity_map[i].y = MAP_HEIGHT / 4 + rand() % 10 - 5;
-		entity_map[i].symbol = 'h';
-		entity_map[i].color = DARK_YELLOW;
 	}
 }
 
@@ -215,6 +186,20 @@ void UpdateEntities()
 	}
 }
 
+
+void PrintEnvironmentMap()
+{
+	for (int i = 0; i < MAP_HEIGHT; i++)
+		for (int j = 0; j < MAP_WIDTH; j++)
+			PutObjectToBuffer(j, i + 6, environment_map[i][j].object);
+}
+
+void PrintEntities()
+{
+	for (int i = 0; i < ENTITY_CAPACITY; i++)
+		PutObjectToBuffer(entity_map[i].x, entity_map[i].y + 6, entity_map[i].object);
+}
+
 void PrintUI()
 {
 	for (int i = 0; i < 6; i++)
@@ -222,21 +207,22 @@ void PrintUI()
 		{
 			if ((i == 0 && (j == 0 || j == MAP_WIDTH - 1)) ||
 				(i == 5 && (j == 0 || j == MAP_WIDTH - 1)))
-				SetBuffer(j, i, '#', WHITE);
+				PutObjectToBuffer(j, i, (Tobject){'#', WHITE});
 			else if (j == 0 || j == MAP_WIDTH - 1)
-				SetBuffer(j, i, '|', WHITE);
+				PutObjectToBuffer(j, i, (Tobject){'|', WHITE});
 			else if (i == 0 || i == 5)
-				SetBuffer(j, i, '-', WHITE);
+				PutObjectToBuffer(j, i, (Tobject){'-', WHITE});
 			else
-				SetBuffer(j, i, ' ', WHITE);
+				PutObjectToBuffer(j, i, (Tobject){' ', WHITE});
 		}
 
 	for (int i = 0; i < 8; i++)
-		SetBuffer(3 + i % 4 * 2, 2 + i / 4, '.', WHITE);
+		PutObjectToBuffer(3 + i % 4 * 2, 2 + i / 4, (Tobject){'.', WHITE});
 
-	SetBuffer(2 + inventory_id % 4 * 2, 2 + inventory_id / 4, '[', WHITE);
-	SetBuffer(2 + inventory_id % 4 * 2 + 2, 2 + inventory_id / 4, ']', WHITE);
+	PutObjectToBuffer(2 + inventory_id % 4 * 2, 2 + inventory_id / 4, (Tobject){'[', WHITE});
+	PutObjectToBuffer(2 + inventory_id % 4 * 2 + 2, 2 + inventory_id / 4, (Tobject){']', WHITE});
 }
+
 
 int main()
 {
@@ -246,7 +232,7 @@ int main()
 
 	SetWindowSize();
 
-	BuildEnviromentMap();
+	BuildEnvironmentMap();
 	BuildEntityMap();
 
 	const int FPS = 12;
@@ -260,14 +246,15 @@ int main()
 		frameStart = clock();
 
 		PrintUI();
-		PrintEnviromentMap();
+		PrintEnvironmentMap();
+		PrintEntities();
 
 		SetCursorPosition(0, 0);
 		for (int i = 0; i < CONSOLE_HEIGHT; i++)
 		{
 			for (int j = 0; j < CONSOLE_WIDTH; j++)
 				if (NEED_TO_UPDATE[i][j] == TRUE)
-					PrintSymbol(j, i, MY_BUFFER[i][j].symbol, MY_BUFFER[i][j].color);
+					PutObjectToConsole(j, i, MY_BUFFER[i][j]);
 		}
 
 		if (GetAsyncKeyState(VK_W)) MoveEntity(&entity_map[0], 0, -1);
@@ -279,19 +266,17 @@ int main()
 
 		UpdateEntities();
 
-		SetCursorPosition(0, 0);
-		SetConsoleTextAttribute(MY_HANDLE, GREEN);
 		frameTime = clock() - frameStart;
 
+		SetCursorPosition(0, 0);
+		SetConsoleTextAttribute(MY_HANDLE, GREEN);
 		if (frameDelay > frameTime)
 		{
 			Sleep(frameDelay - frameTime);
 			printf("%3d", FPS);
 		}
 		else
-		{
 			printf("%3d", (int)(1000 / frameTime));
-		}
 	}
 
 	return 0;
